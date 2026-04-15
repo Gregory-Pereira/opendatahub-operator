ff8b6092c5e1ee9925e3f645e3fc9d5d5c750ade
Sum Huynh <shuynh@redhat.com>
2026-04-15 14:25:17 -0400

    scale down dashboard-redirect

--- docs/install/wva/06-scale-down-non-essential.sh
+++ docs/install/wva/06-scale-down-non-essential.sh
@@ -25,6 +25,9 @@ echo "  Scaled down openshift-monitoring/alertmanager-main"
 kubectl scale deployment rhods-operator -n redhat-ods-operator --replicas=1
 echo "  Scaled rhods-operator to 1 replica (from 3)"
 
+kubectl scale deployment dashboard-redirect -n opendatahub --replicas=1
+echo "  Scaled dashboard-redirect to 1 replica"
+
 if kubectl get deployment lws-controller-manager -n openshift-lws-operator &>/dev/null; then
   kubectl scale deployment lws-controller-manager -n openshift-lws-operator --replicas=1
   echo "  Scaled down openshift-lws-operator/lws-controller-manager"
